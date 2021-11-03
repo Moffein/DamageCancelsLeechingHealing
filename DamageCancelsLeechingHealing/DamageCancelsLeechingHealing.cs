c31fd25020b443eb572eb04c98fd6ed9593e0bf6
Moffein <22742925+Moffein@users.noreply.github.com>
2021-11-03 13:59:29 -0700

    Add project files.

---
+++ DamageCancelsLeechingHealing/DamageCancelsLeechingHealing.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using UnityEngine.Networking;
+
+namespace DamageCancelsLeechingHealing
+{
+    [BepInDependency("com.swuff.LostInTransit")]
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("com.Moffein.DamageCancelsLeechingHealing", "Damage Cancels Leeching Healing", "1.0.0")]
+    public class DamageCancelsLeechingHealing : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            On.RoR2.HealthComponent.TakeDamage += (orig, self, damageInfo) =>
+            {
+                orig(self, damageInfo);
+                if (NetworkServer.active && !damageInfo.rejected)
+                {
+                    if (self.body && self.body.HasBuff(LostInTransit.Buffs.LeechingRegen.buff))
+                    {
+                        self.body.ClearTimedBuffs(LostInTransit.Buffs.LeechingRegen.buff);
+                    }
+                }
+            };
+        }
+    }
+}
